101e5789163445f44e8b72aa3c7a52d49c866cdd
Scott Kurz <skurz@us.ibm.com>
2018-11-09 13:56:49 -0500

    Fix remaining boost-gradle issues

---
+++ boost-maven/boost-maven-plugin/src/it/test-docker/src/test/java/application/DockerPushIT.java
@@ -0,0 +1,16 @@
+org.eclipse.m2e.logback.configuration: The org.eclipse.m2e.logback.configuration bundle was activated before the state location was initialized.  Will retry after the state location is initialized.
+org.eclipse.m2e.logback.configuration: Logback config file: C:\Users\ScottKurz\eclipse-workspace\.metadata\.plugins\org.eclipse.m2e.logback.configuration\logback.1.9.1.20180912-1601.xml
+org.eclipse.m2e.logback.configuration: Initializing logback
+
+Usage: eclipse -application org.eclipse.jdt.core.JavaCodeFormatter [ OPTIONS ] -config <configFile> <files>
+
+   <files>   Java source files and/or directories to format.
+             Only files ending with .java will be formatted in the given directory.
+   -config <configFile> Use the formatting style from the specified properties file.
+                        Refer to the help documentation to find out how to generate this file.
+
+ OPTIONS:
+
+   -help                Display this message.
+   -quiet               Only print error messages.
+   -verbose             Be verbose about the formatting job.
